d85a5ad0f620853a8d5dcf465f975cf4bebffa66
moodyzz95 <43550290+moodyzz95@users.noreply.github.com>
2019-03-03 23:11:03 +0300

    Testing classes
    we add the doses times array and doses number and medicine name

--- classes/classes.ino
+++ classes/classes.ino
@@ -1,9 +1,38 @@
+#include "medicine.h"
+#include "user.h"
+ medicine med[2];
+  User user;
 void setup() {// main class , will add later 
+  Serial.begin(9600);
+  user.setID(1);
+  user.setUname("Ahmed");
+  med[0].setMedID(1);
+  med[0].setMedName("Panadol");
+  med[0].setType("Pills");
+  med[0].setDosage("1 pill");
+  med[0].setStockState(true);
+  med[0].setTakenState(false);
+  med[0].setDosesNum(3);
+  for(int i = 0; i<med[0].getDosesNum();i++){
+  med[0].setTimes("11",i);
+  }
   
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println(user.getID());
+  Serial.println(user.getUname());
+  Serial.println(med[0].getMedID());
+  Serial.println(med[0].getMedName());
+  Serial.println(med[0].getType());
+  Serial.println(med[0].getDosage());
+  Serial.println(med[0].getStockState());
+  Serial.println(med[0].getTakenState());
+  
+  Serial.println(med[0].getDosesNum());
+  
+  for(int i = 0; i<med[0].getDosesNum();i++){
+  Serial.println(med[0].getTimes(i));
+  }
+delay(30000);
 }
--- classes/medicine.h
+++ classes/medicine.h
@@ -1,10 +1,13 @@
 class medicine {
   private:
   int MedID;// medicine id
+  String MedName; // name of the medicine
   String Type;// type of the medicine. ex:syrup , pill
   String Dosage;// the amount of each dose
+  int DosesNum; // number of doses per day
   boolean InStock;// to check if the medicine still available or not
   boolean Taken;//to check if the user took his medicine or not
+  String Times[];// to store the times for single medicine
 
    public:
           void setMedID(int id){
@@ -12,6 +15,12 @@ class medicine {
           }
           int getMedID(){
             return MedID;
+          }
+          void setMedName(String name){
+            MedName=name;
+          }
+          String getMedName(){
+            return MedName;
           }
           void setType(String type){
             Type=type;
@@ -25,6 +34,13 @@ class medicine {
           String getDosage(){
             return Dosage;
           }
+          void setDosesNum(int num){
+            DosesNum=num;
+            Times[DosesNum];
+          }
+          int getDosesNum(){
+            return DosesNum;
+          }
           void setStockState(boolean state){
             InStock=state;
           }
@@ -37,6 +53,13 @@ class medicine {
           boolean getTakenState(){
             return Taken;
           }
+          void setTimes(String times, int i){
+            Times[i]=times;
+          }
+          String getTimes(int i){
+            return Times[i];
+          }
+          
           
 };
 
