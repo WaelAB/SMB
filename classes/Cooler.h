85824210b95997aa8d46c55fa3edf7b4fe49647f
WaelAB <43557459+WaelAB@users.noreply.github.com>
2019-03-10 02:13:07 +0300

    Add confirmation after receiving data + add the cooler class to run the fridge

---
+++ classes/Cooler.h
@@ -0,0 +1,23 @@
+
+class Cooler {
+public:
+    int relayPin = 8; // Digital pin 8 for the relay
+    int tempPin = A1; // Analog pin A1 for the temperature sensor
+    double temp = 0; // initilaze the temp degree
+
+    // the setup function runs once when you press reset or power the board
+
+
+
+  public:
+    void CheckTemp () {
+      temp = analogRead(tempPin); //takes the read from the analog pin
+      temp = temp * 0.48828125; // converting the analog read to Celsius
+      //Serial.println(temp);
+      if (temp > 24) { // if the temperature is more than 24
+        digitalWrite(relayPin, LOW); // turn the cooler on
+      } else  { // if the temprature is less than 18
+        digitalWrite(relayPin, HIGH);// Turn the cooler off
+      }
+    }
+};
--- classes/classes.ino
+++ classes/classes.ino
@@ -3,15 +3,20 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "HX711.h"
+#include "Cooler.h"
 
 HX711 scale(3, 2);// digital pins
 float calibration_factor = 2000; // this calibration factor is adjusted according to my load cell
 float units;// where the wight is stored
 
+boolean reply = false;
+
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-medicine med[2];// create array object of medicine type
-User user[2];
+medicine med[2];// create array of medicine type
+User user[2]; // create array of medicine type
+Cooler cool; // create object of cooler type
+
 String uName ; // User Name
 int uID; // User ID
 String MedName; // Medicine Name
@@ -33,8 +38,100 @@ void setup() {
   lcd.backlight(); // Trun the light of the screen
   Wire.begin(8);                // join i2c bus with address 8 to the esp
   Wire.onReceive(receiveEvent); // register receive event from esp
-  // Wire.onRequest(requestEvent); // send event to esp
+  Wire.onRequest(confirm); // send confirmation to esp
+  pinMode(cool.relayPin, OUTPUT);
+  digitalWrite(cool.relayPin, LOW);
 
+  SettingUp();
+
+}
+
+void loop() {
+
+  cool.CheckTemp();
+  receiveEvent();
+
+}
+
+// function that executes whenever data is received from master
+
+void receiveEvent() {
+
+
+  while (0 < Wire.available()) {
+
+
+    uID = Wire.read(); // Receive and store the user ID from the esp
+
+    user[uID].setID(uID);
+    Serial.print("User ID: ");
+    Serial.println(user[uID].getID());
+    //*******************
+
+    strLen = Wire.read();
+    for (int i = 0; i < strLen ; i++) {
+      c = Wire.read();
+      uName += c;
+    }
+    user[uID].setUname(uName);
+    Serial.println(user[uID].getUname());
+    uName = "";
+    //*******************
+
+    ConNum = Wire.read();
+    med[ConNum].setContainerNum(ConNum);
+    Serial.println(med[ConNum].getContainerNum());
+    //*******************
+
+    strLen = Wire.read();
+    for (int i = 0; i < strLen ; i++) {
+      c = Wire.read(); // Receive and store the Medicine name value char by char
+      MedName += c;   // And store it into the Dosage varriable
+    }
+
+    med[ConNum].setMedName(MedName);
+    Serial.println("MedName: " + med[ConNum].getMedName());
+    MedName = "";
+    //*******************
+
+    strLen = Wire.read();
+    for (int i = 0; i < strLen ; i++) {
+      c = Wire.read(); // Receive and store the Dosage amount value char by char
+      Dosage += c;    // And store it into the Dosage varriable
+    }
+    med[ConNum].setDosage(Dosage);
+    Serial.println(med[ConNum].getDosage());
+    Dosage = "";
+    //*******************
+
+    DosageNum = Wire.read(); // Receive and store the number of doses value from the esp
+    Serial.println(DosageNum);
+    for (int i = 0 ; i < DosageNum ; i++) {
+      DTime = Wire.read(); // Receive and store the Dosage time value from the esp
+      med[ConNum].setTimes(DTime, i);
+      Serial.print("Time: ");
+      Serial.println(med[ConNum].getTimes(i));
+    }
+    //*******************
+
+    InStock = Wire.read(); // Read the In stock value from the esp
+    med[ConNum].setStockState(InStock);
+    Serial.print("In Stock ? ");
+    Serial.println(med[ConNum].getStockState());
+
+   confirm();
+
+    Serial.println();            //  to newline
+    user[uID].setMed(med[ConNum]);
+  }
+}
+
+void confirm() {// this function will send confirmation to esp
+  int GOTIT = 1;// for getting the data
+  Wire.write(GOTIT);
+}
+
+void SettingUp() {
   scale.set_scale(); //
   scale.tare();  //Reset the scale to 0
   long zero_factor = scale.read_average(); // to remove the need of tare scale
@@ -52,102 +149,26 @@ void setup() {
   lcd.print(" a medicine");
   delay(3000);
   lcd.clear();
-
-  scale.set_scale(calibration_factor); //Adjust to this calibration factor
-
-  units = scale.get_units(), 10;
-  while (units < 0.10) {// Wait untill the user to put his medicine
+  /*
+    scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
     units = scale.get_units(), 10;
-    if (units < 0)
-    {
-      units = 0.00;// to avoid getting minus weight values
+    while (units < 0.10) {// Wait untill the user to put his medicine
+
+      units = scale.get_units(), 10;
+      if (units < 0)
+      {
+        units = 0.00;// to avoid getting minus weight values
+      }
     }
-  }
 
-  firstWeight = units;
-  Serial.print(firstWeight);
-  Serial.print(" grams");
-
+    firstWeight = units;
+    Serial.print(firstWeight);
+    Serial.print(" grams");
+  */
 
 
 }
-
-void loop() {
-  receiveEvent();
-}
-
-// function that executes whenever data is received from master
-
-void receiveEvent() {
-
-
-
-  while (0 < Wire.available()) {
-
-    uID = Wire.read(); // Receive and store the user ID from the esp
-
-    user[uID].setID(uID);
-    //Serial.print("User ID: ");
-    //Serial.println(user[uID].getID());
-    //*******************
-
-    strLen = Wire.read();
-    for (int i = 0; i < strLen ; i++) {
-      c = Wire.read();
-      uName += c;
-    }
-    user[uID].setUname(uName);
-    //Serial.println(user[uID].getUname());
-    uName = "";
-    //*******************
-
-    ConNum = Wire.read();
-    med[ConNum].setContainerNum(ConNum);
-    //Serial.println(med[ConNum].getContainerNum());
-    //*******************
-
-    strLen = Wire.read();
-    for (int i = 0; i < strLen ; i++) {
-      c = Wire.read(); // Receive and store the Medicine name value char by char
-      MedName += c;   // And store it into the Dosage varriable
-    }
-
-    med[ConNum].setMedName(MedName);
-    // Serial.println("MedName: " + med[ConNum].getMedName());
-    MedName = "";
-    //*******************
-
-    strLen = Wire.read();
-    for (int i = 0; i < strLen ; i++) {
-      c = Wire.read(); // Receive and store the Dosage amount value char by char
-      Dosage += c;    // And store it into the Dosage varriable
-    }
-    med[ConNum].setDosage(Dosage);
-    // Serial.println(med[ConNum].getDosage());
-    Dosage = "";
-    //*******************
-
-    DosageNum = Wire.read(); // Receive and store the number of doses value from the esp
-    //Serial.println(DosageNum);
-    for (int i = 0 ; i < DosageNum ; i++) {
-      DTime = Wire.read(); // Receive and store the Dosage time value from the esp
-      med[ConNum].setTimes(DTime, i);
-      //Serial.print("Time: ");
-      //Serial.println(med[ConNum].getTimes(i));
-    }
-    //*******************
-
-    InStock = Wire.read(); // Read the In stock value from the esp
-    med[ConNum].setStockState(InStock);
-    //Serial.print("In Stock ? ");
-    //Serial.println(med[ConNum].getStockState());
-
-    //Serial.println();            //  to newline
-    user[uID].setMed(med[ConNum]);
-  }
-}
-
 
 // Will use this later
 // function that executes whenever data is requested from master
