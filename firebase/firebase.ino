f9830e78c693836c0f67efcc67f88fb16eaf0f54
moodyzz95 <43550290+moodyzz95@users.noreply.github.com>
2019-03-16 23:31:32 +0300

    fixing handshaking and exception handling
    handshaking now depend of  return value of endTransmission method which it will return Zero if the transmission sucsses

--- firebase/firebase.ino
+++ firebase/firebase.ino
@@ -8,6 +8,8 @@
 #define WIFI_SSID "Moodyzz" // name of the router 
 #define WIFI_PASSWORD "14161416" // password of the router
 int len;
+int takenTime=0;
+int confirm = -1;
 void setup() {
   Serial.begin(9600);
   Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
@@ -23,10 +25,11 @@ void setup() {
   Serial.println(WiFi.localIP());// IP address
   //connect to FireBase
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Wire.beginTransmission(8);
+  //Wire.beginTransmission(8);
 
-  int confirm = 0;
+  
   int responce = 0;
+  int takenTime=0;
   /**while (responce == 0) {
     Wire.requestFrom(8, 1);
     while (Wire.available()) {
@@ -40,21 +43,22 @@ void setup() {
     Wire.write(1);**/
 
 
-  while (confirm == 0) {
+  while (true) {
     Serial.println("confirm loop");
     
     SettingUp();
-    delay(1000);
-    Wire.requestFrom(8, 1);
-    while (Wire.available()) {
+    delay(2000);
+    //Wire.requestFrom(8, 4);
+   // while (Wire.available()) {
       
       Serial.println("responce nested loop");
-      confirm = Wire.read();
-      Serial.print(confirm);
-      if (confirm == 1) {
+      
+      Serial.print("confirm: ");
+      Serial.println(confirm);
+      if (confirm == 0) {
         break;
       }
-    }
+    //}
 
   }
 }
@@ -67,13 +71,17 @@ void loop() {
     SettingUp();
 
     Firebase.setBool("/Flag", false);
-    while (Wire.available()) {
-      int takenTime = Wire.read();
-      boolean state = Wire.read();
-      String name = Firebase.pushInt("/logs", takenTime);
+    
 
     }
-  }
+ /**   Wire.requestFrom(8, 1);
+    while (Wire.available()) {
+      takenTime = Wire.read();
+      Serial.print("log:");
+      Serial.println(takenTime);
+     // boolean state = Wire.read();
+     Firebase.pushInt("/taken", takenTime);
+  }**/
 
 }
 void SettingUp() {
@@ -81,8 +89,16 @@ void SettingUp() {
   //getting information from the data base
   Wire.beginTransmission(8);
   int userID = Firebase.getInt("/User/ID");
+  if (Firebase.failed()) { // check for error
+    Serial.print("setting /number failed:");
+    Serial.println(Firebase.error());
+  }
   Wire.write(userID);
   String uName = Firebase.getString("/User/Name"); // getting the username
+  if (Firebase.failed()) { // check for error
+    Serial.print("setting /number failed:");
+    Serial.println(Firebase.error());
+  } 
   len = uName.length(); // lenght of the username
 
   Wire.write(len); // sending the lenght of the username
@@ -91,8 +107,16 @@ void SettingUp() {
   }
 
   int containerNum = Firebase.getInt("/Medicine/container");
+  if (Firebase.failed()) { // check for error
+    Serial.print("setting /number failed:");
+    Serial.println(Firebase.error());
+  }
   Wire.write(containerNum);
   String MedName = Firebase.getString("/Medicine/Name");
+  if (Firebase.failed()) { // check for error
+    Serial.print("setting /number failed:");
+    Serial.println(Firebase.error());
+  }
   len = MedName.length();
   Wire.write(len);
   for (int i = 0; i < len; i++) {
@@ -101,6 +125,10 @@ void SettingUp() {
 
 
   String Dosage = Firebase.getString("/Medicine/Dosage");
+  if (Firebase.failed()) { // check for error
+    Serial.print("setting /number failed:");
+    Serial.println(Firebase.error());
+  }
   len = Dosage.length();
 
   Wire.write(len);
@@ -111,6 +139,10 @@ void SettingUp() {
 
 
   int DosageNum = Firebase.getInt("/Medicine/DosesNum");
+  if (Firebase.failed()) { // check for error
+    Serial.print("setting /number failed:");
+    Serial.println(Firebase.error());
+  }
   Serial.println(DosageNum);
   Wire.write(DosageNum);
   String d = "/Medicine/Times/Dose";
@@ -119,11 +151,19 @@ void SettingUp() {
   for (int i = 0; i < DosageNum; i++) {
     String Dosepath = d + i;
     int fDose = Firebase.getInt(Dosepath);
+    if (Firebase.failed()) { // check for error
+    Serial.print("setting /number failed:");
+    Serial.println(Firebase.error());
+  }
     Wire.write(fDose);
     Serial.print(fDose);
 
   }
   boolean inStock = Firebase.getBool("/Medicine/InStock");
+  if (Firebase.failed()) { // check for error
+    Serial.print("setting /number failed:");
+    Serial.println(Firebase.error());
+  }
   Wire.write(inStock);
-  Wire.endTransmission();
+ confirm = Wire.endTransmission();
 }
